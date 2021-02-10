4f6340b608119895db8f43b4d0af03d545b478d8
james <james@jamesfinnerty.co.uk>
2021-02-10 12:22:22 +0000

    Project config and a basic call setup

---
+++ Sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sandbox.FSharp;
+
+namespace Sandbox.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = FSharp.testFunctions.factorial(3);
+
+            Console.WriteLine("C Sharp Main Called");
+            Console.WriteLine("Factorial 3: " + answer.ToString());
+        }
+    }
+}
---
+++ SandboxFSharp/Program.fs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox.FSharp
+
+open System
+
+module dataSets =
+    let numberList = [ 1 .. 100 ]
+
+module testFunctions =
+
+    let rec factorial n =
+        match n with
+        | 0 | 1 -> 1
+        | _ -> n * factorial(n-1)
+
+    let results = dataSets.numberList |> List.map (fun n -> factorial n)
+        
+        
